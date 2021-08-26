f6cfbde2c0a9a38ab669b4ed8609e33a14e0ed5d
ssapienza <sapienza@umd.edu>
2021-08-26 16:25:25 -0400

    Add files via upload

---
+++ sapienza-ucla-portfolio/Paper_Excerpts/Entries/2008/3/14_Light_My_Fire__Video_Furnace_and_online_access_for_moving_image_archives_files/14_Light_My_Fire__Video_Furnace_and_online_access_for_moving_image_archives.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('14_Light_My_Fire__Video_Furnace_and_online_access_for_moving_image_archives_files/14_Light_My_Fire__Video_Furnace_and_online_access_for_moving_image_archivesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
