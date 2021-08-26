48a364a580fdee92aa96e5823a6ded6ce06aa817
ssapienza <sapienza@umd.edu>
2021-08-26 16:12:47 -0400

    More portfolio files upload

---
+++ sapienza-ucla-portfolio/Website_Projects/Entries/2007/3/23_Disney_Restoration_Project_files/23_Disney_Restoration_Project.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('23_Disney_Restoration_Project_files/23_Disney_Restoration_ProjectMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ sapienza-ucla-portfolio/Website_Projects/Website_Projects_files/Website_Projects.js
@@ -0,0 +1,13 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("https://www.stephaniesapienza.com/sapienza-ucla-portfolio/Website_Projects/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Website_Projects_files/Website_ProjectsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
