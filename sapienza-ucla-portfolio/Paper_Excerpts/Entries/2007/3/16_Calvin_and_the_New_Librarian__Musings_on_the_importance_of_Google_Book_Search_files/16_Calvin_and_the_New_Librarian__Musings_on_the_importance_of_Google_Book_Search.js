4fa6b62df19768e1b587d64281f8baca263a3995
ssapienza <sapienza@umd.edu>
2021-08-26 16:21:42 -0400

    Add files via upload

---
+++ sapienza-ucla-portfolio/Paper_Excerpts/Entries/2007/3/16_Calvin_and_the_New_Librarian__Musings_on_the_importance_of_Google_Book_Search_files/16_Calvin_and_the_New_Librarian__Musings_on_the_importance_of_Google_Book_Search.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('16_Calvin_and_the_New_Librarian__Musings_on_the_importance_of_Google_Book_Search_files/16_Calvin_and_the_New_Librarian__Musings_on_the_importance_of_Google_Book_SearchMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
