c27c9febabe6d29e2de8dd74643b2a6276b41976
ssapienza <sapienza@umd.edu>
2021-08-26 16:14:41 -0400

    Portfolio Site Practicum Podcasts upload

---
+++ sapienza-ucla-portfolio/Practicum_Podcasts_files/Practicum_Podcasts.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Practicum_Podcasts_files/Practicum_PodcastsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
