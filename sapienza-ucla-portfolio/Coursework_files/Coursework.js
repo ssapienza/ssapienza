8984ebeefd1f14f2f77d04f5c1e0f26318716c57
ssapienza <sapienza@umd.edu>
2021-08-26 15:46:05 -0400

    UCLA Portfolio site pages, Coursework, Media files

---
+++ sapienza-ucla-portfolio/Coursework_files/Coursework.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20210826
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Coursework_files/CourseworkMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
